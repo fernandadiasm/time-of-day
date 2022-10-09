a4984f75c5458517c2d970d8b1dbfe4988745daf
Fernanda.dias97@hotmail.com <fernanda.dias97@hotmail.com>
2022-10-09 00:06:12 -0300

    build: :sparkles: adiciona mudança de cores

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 function carregar() {
     let msg = document.querySelector('div#msg')
     let img = document.querySelector('img#imagem')
+    let title = document.querySelector('h1.title')
     let data = new Date()
     let hora = data.getHours()
     let min = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora}:${min}.`
+    msg.innerHTML = `It's ${hora}:${min}.`
+
+    hora = 15
 
     if (hora >= 5 && hora < 12) {
         img.src = './assets/img/morning.png'
         document.body.style.background = '#edd4b7'
         document.querySelector('section#sessao').style.background = "#f0e9de"
-        msg.innerHTML = `Agora são ${hora}:${min} <br>Bom dia!`
-        msg.style = 'color: #038be1'
+        msg.innerHTML = `It's ${hora}:${min} a.m. <br>Good morning!`
+        msg.style = 'color: #614035'
+        title.innerHTML = `Time of Day`
+        title.style = 'color: #273940'
 
     } else if (hora >= 12 && hora <= 18) {
         img.src = './assets/img/afternoom.png'
         document.body.style.background = '#326c92'
         document.querySelector('section#sessao').style.background = "#94b7d8"
-        msg.innerHTML = `Agora são ${hora}:${min} <br>Boa tarde!`
+        msg.innerHTML = `It's  ${hora}:${min} p.m. <br>Good afternoon!`
+        msg.style = 'color: #2c231c'
+
 
     } else {
         img.src = './assets/img/night.png'
         document.body.style.background = '#0f1841'
         document.querySelector('section#sessao').style.background = "#a6aef8"
-        msg.innerHTML = `Agora são ${hora}:${min} <br>Boa noite!`
+        msg.innerHTML = `It's ${hora}:${min} p.m. <br>Good evening!`
+        msg.style = 'color: #222025'
     }
 }
 
